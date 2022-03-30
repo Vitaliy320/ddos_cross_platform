d4823c00d1d1e0797df5c86afac4531286916615
Vitalii Borysov <vitaliy1315320@gmail.com>
2022-03-30 11:04:02 +0300

    Initial commit

---
+++ ddos_cross_platform/ddos_cross_platform.cpp
@@ -0,0 +1,12 @@
+﻿// ddos_cross_platform.cpp : Defines the entry point for the application.
+//
+
+#include "ddos_cross_platform.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
