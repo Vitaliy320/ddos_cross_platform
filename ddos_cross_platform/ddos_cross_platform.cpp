3eb39676301af278afb2936cba654241f55df6f4
Vitalii Borysov <vitaliy1315320@gmail.com>
2022-03-31 11:11:09 +0300

    You are a bold done

--- ddos_cross_platform/ddos_cross_platform.cpp
+++ ddos_cross_platform/ddos_cross_platform.cpp
@@ -7,6 +7,11 @@
 
 using namespace std;
 
+string removeSpaces(string str) {
+	str.erase(remove(str.begin(), str.end(), ' '), str.end());
+	return str;
+}
+
 int main()
 {
 	Functions functions;
@@ -14,6 +19,13 @@ int main()
 	cout << custom_sum(2, 3) << endl;*/
 	string input_message;
 	cout << "Hello there!" << endl;
-	cin >> input_message;
+	getline(cin, input_message);
+	input_message = removeSpaces(input_message);
+	
+	if (input_message == "GeneralKenobi" || input_message == "GeneralKenobi!") {
+		cout << "You are a bold one" << endl;
+		getline(cin, input_message);
+	}
+	
 	return 0;
 }
